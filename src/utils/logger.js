57bc0d58028b534d07d968191a8ed7e22c3430b4
voron69-bit <wolvenstudio@gmail.com>
2025-12-09 11:10:39 -0500

    Create logger.js

---
+++ src/utils/logger.js
@@ -0,0 +1,44 @@
+const winston = require('winston');
+const path = require('path');
+const config = require('../config');
+
+const logFormat = winston.format.combine(
+  winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  winston.format.errors({ stack: true }),
+  winston.format.printf(({ level, message, timestamp, stack, ...meta }) => {
+    let log = `${timestamp} [${level.toUpperCase()}]: ${message}`;
+    if (Object.keys(meta).length > 0) {
+      log += ` ${JSON.stringify(meta)}`;
+    }
+    if (stack) {
+      log += `\n${stack}`;
+    }
+    return log;
+  })
+);
+
+const logger = winston.createLogger({
+  level: config.logging.level,
+  format: logFormat,
+  transports: [
+    new winston.transports.Console({
+      format: winston.format.combine(
+        winston.format.colorize(),
+        logFormat
+      ),
+    }),
+    new winston.transports.File({
+      filename: path.join(config.paths.logs, 'error.log'),
+      level: 'error',
+      maxsize: 10 * 1024 * 1024, // 10MB
+      maxFiles: 5,
+    }),
+    new winston.transports.File({
+      filename: path.join(config.paths.logs, 'combined.log'),
+      maxsize: 10 * 1024 * 1024, // 10MB
+      maxFiles: 5,
+    }),
+  ],
+});
+
+module.exports = logger;
